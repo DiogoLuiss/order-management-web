f095f0a07836654d28de00cb99178b1fd61f6186
Diogo <diogoluisilva08@gmail.com>
2025-09-14 12:05:14 -0300

    improving cart interaction

--- src/js/components/toast.js
+++ src/js/components/toast.js
@@ -1,10 +1,16 @@
 function showToast(message, type = 'info', duration = 3000) {
     const toastId = `toast-${Date.now()}`;
+    const zIndex = 'z-index: 9999;';
+
     const toast = $(`
-        <div id="${toastId}" class="toast align-items-center text-bg-${type} border-0 position-fixed top-0 end-0 mt-5 me-3" role="alert" aria-live="assertive" aria-atomic="true">
+        <div id="${toastId}" 
+             class="toast align-items-center text-bg-${type} border-0 position-fixed top-0 end-0 mt-5 me-3" 
+             role="alert" aria-live="assertive" aria-atomic="true"
+             style="${zIndex}">
             <div class="d-flex">
                 <div class="toast-body">${message}</div>
-                <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                <button type="button" class="btn-close btn-close-white me-2 m-auto" 
+                        data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
         </div>
     `);
