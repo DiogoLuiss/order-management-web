59b7d2b688b6f73138192c0edc70e0faf7c17014
Diogo <diogoluisilva08@gmail.com>
2025-09-13 21:28:23 -0300

    Adjusting design

--- src/js/components/toast.js
+++ src/js/components/toast.js
@@ -1,15 +1,20 @@
 function showToast(message, type = 'info', duration = 3000) {
     const toastId = `toast-${Date.now()}`;
     const toast = $(`
-        <div id="${toastId}" class="toast align-items-center text-bg-${type} border-0 position-fixed bottom-0 end-0 m-3" role="alert" aria-live="assertive" aria-atomic="true">
+        <div id="${toastId}" class="toast align-items-center text-bg-${type} border-0 position-fixed top-0 end-0 mt-5 me-3" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="d-flex">
                 <div class="toast-body">${message}</div>
                 <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
         </div>
     `);
+
     $('body').append(toast);
+
     const bsToast = new bootstrap.Toast(toast[0], { delay: duration });
     bsToast.show();
-    toast.on('hidden.bs.toast', function () { $(this).remove(); });
+
+    toast.on('hidden.bs.toast', function () {
+        $(this).remove();
+    });
 }
